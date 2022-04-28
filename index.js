ef4aa402264d2ee88dbf3c27aeea791653b464ab
Dimitrii <32913446+DimaIpatii@users.noreply.github.com>
2022-04-28 14:36:37 +0200

    message

---
+++ index.js
@@ -0,0 +1,10 @@
+import {HOMEPAGE_URL, GALLERY_URL, FORM_URL} from './shared/variables';
+
+const homeLink = document.createElement("a");
+homeLink.setAttribute("href", HOMEPAGE_URL);
+
+const galleryLink = document.createElement("a");
+galleryLink.setAttribute("href", GALLERY_URL);
+
+const formLink = document.createElement("a");
+formLink.setAttribute("href", FORM_URL);
